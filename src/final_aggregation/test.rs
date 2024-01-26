47061583489700ad20ec6b7b4551f6d26f66b8d5
RequiemOfSouls <RequiemOfSouls@zklink.org>
2024-01-26 10:40:11 +0800

    test: optimize test cases

--- src/final_aggregation/test.rs
+++ src/final_aggregation/test.rs
@@ -1,14 +1,16 @@
 use std::collections::BTreeMap;
-use crate::block_aggregation::bellman::plonk::better_better_cs::cs::Circuit as _;
+use std::path::PathBuf;
 use zklink_oracle::ZkLinkOracle;
 use advanced_circuit_component::franklin_crypto::bellman::bn256::Bn256;
 use advanced_circuit_component::franklin_crypto::bellman::plonk::better_better_cs::cs::PlonkCsWidth4WithNextStepAndCustomGatesParams;
 use crate::tests::generate_test_constraint_system;
 use crate::{FinalAggregationCircuit, OracleAggregationCircuit, OracleOutputDataWitness, UniformProof, UniformVerificationKey, OraclePricesCommitmentWitness};
+use crate::bellman::bn256::Fr;
+use crate::bellman::plonk::better_better_cs::cs::Circuit;
+use crate::bellman::plonk::commitments::transcript::keccak_transcript::RollingKeccakTranscript;
 use crate::params::{COMMON_CRYPTO_PARAMS, RescueTranscriptForRecursion};
 
-#[test]
-fn test_final_aggregation_circuit() {
+fn create_test_final_aggregation_circuit() -> FinalAggregationCircuit<'static, Bn256> {
     use crate::OracleCircuitType::*;
 
     println!("---------------------------oracle circuit start--------------------------------");
@@ -85,16 +87,47 @@ fn test_final_aggregation_circuit() {
     let oracle_agg_vk: UniformVerificationKey<Bn256> = unsafe { std::mem::transmute(oracle_agg_vk) };
     let block_agg_proof: UniformProof<Bn256> = unsafe { std::mem::transmute(proof) };
     let block_agg_vk: UniformVerificationKey<Bn256> = unsafe { std::mem::transmute(vk) };
-    let oracle_aggregation_circuit = FinalAggregationCircuit::generate(
+    FinalAggregationCircuit::generate(
         block_output_data,
         (0, block_agg_proof),
         BTreeMap::from([(0, block_agg_vk)]),
         vec![oracle_agg_output],
         vec![(0, oracle_aggregation_proof)],
         BTreeMap::from([(0, oracle_agg_vk)]),
-    );
+    )
+}
 
+#[test]
+fn test_final_aggregation_circuit_satisfied() {
+    let final_aggregation_circuit = create_test_final_aggregation_circuit();
     let mut cs = generate_test_constraint_system();
-    oracle_aggregation_circuit.synthesize(&mut cs).unwrap();
+    final_aggregation_circuit.synthesize(&mut cs).unwrap();
     assert!(cs.is_satisfied());
+}
+
+#[test]
+fn test_final_aggregation_circuit_proving_and_verify() {
+    let final_aggregation_circuit = create_test_final_aggregation_circuit();
+    let (proof, vk ) = circuit_testing::prove_and_verify_circuit_for_params::<
+        Bn256,
+        _,
+        PlonkCsWidth4WithNextStepAndCustomGatesParams,
+        RollingKeccakTranscript<Fr>,
+        true
+    >(
+        final_aggregation_circuit,
+        None
+    ).unwrap();
+
+    let key_path = PathBuf::from(r"./final_aggregation.key");
+    let vk_file =
+        std::fs::File::create(&key_path).expect("can't create file at final verification key path");
+    vk.write(vk_file)
+        .expect("Failed to save final verification key");
+
+    let proof_path = PathBuf::from(r"./final_aggregation.proof");
+    let proof_file =
+        std::fs::File::create(&proof_path).expect("can't create file at final proof path");
+    proof.write(proof_file)
+        .expect("Failed to save final aggregation proof");
 }
